fd060ff1861903f67ea01acceb0735d313e8f359
mehul1908 <135810448+mehul1908@users.noreply.github.com>
2025-05-21 09:19:39 +0530

    intial commit

---
+++ src/main/java/com/resumeradar/ResumeRadarApplication.java
@@ -0,0 +1,13 @@
+package com.resumeradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeRadarApplication.class, args);
+	}
+
+}
